cb18c429840ecf6b7804d9c978c7f4365c0ae5e8
qy286 <quy.doanduy@gmail.com>
2026-04-01 17:35:28 +0700

    a

---
+++ Scripts/Button.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+namespace SL.Scripts;
+
+public partial class CButton : Godot.Button
+{
+    
+  
+}
--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -1,17 +1,44 @@
-using Godot;
-using System;
 using System.Text.Json;
-
+using Godot;
+namespace SL.Scripts;
 public partial class Menu : Node2D
 {
+    WebSocketPeer ws;
     public override void _Ready()
     {
-        var ws = new WebSocketPeer();
+        ws = new();
+        var err = ws.ConnectToUrl("ws://127.0.0.1:8080");
+        GD.Print("Connect result: ", err);
+    }
+    public override void _Process(double delta)
+    {
+        ws.Poll();
 
-        ws.ConnectToUrl("ws://127.0.0.1:8080");
-        var packet = new { action = "ping", data = new { } };
+        var state = ws.GetReadyState();
 
-        ws.SendText(JsonSerializer.Serialize(packet));
+        if (state == WebSocketPeer.State.Open)
+        {
+           
+
+            if (ws.GetAvailablePacketCount() > 0)
+            {
+                var pkt = ws.GetPacket();
+                GD.Print("Received: ", pkt.GetStringFromUtf8());
+            }
+        }
+        else if (state == WebSocketPeer.State.Closed)
+        {
+            GD.Print("Closed: ", ws.GetCloseCode());
+        }
     }
 
+    public  void _On_Btn_Pressed()
+    {
+            var packet = new { action = "ping", data = new { } };
+
+        ws.SendText(JsonSerializer.Serialize(packet));
+
+    }
+    
 }
+
